f8d076789245f2571fece0bcede56aa85b1d9fdd
farp80 <farp80@gmail.com>
2019-08-31 17:02:13 +0000

    apiCreation_1

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from models import db
-#from models import Person
+from models import Contact
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -28,16 +28,44 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/hello', methods=['POST', 'GET'])
-def handle_person():
+@app.route('/contact', methods=['GET', 'POST'])
+def get_contact():
+    if request.method == 'POST':
+        user1 = Contact(
+            first_name="Jorge",
+            email="jorgesolo_my_super@email.com",
+            address="4445 W Main Ave",
+            last_name="Sairf",
+            phone="843-333-5678")
 
-    response_body = {
-        "hello": "world"
-    }
+        db.session.add(user1)
+        db.session.commit()
+        return jsonify(user1.serialize()), 200
+    if request.method == 'GET':
+        people_query = Contact.query.all()
+        all_people = list(map(lambda x: x.serialize(), people_query))
+        people_array = []
 
-    return jsonify(response_body), 200
+        for one_people in all_people:
+            people_array.append(one_people)
+        return jsonify(people_array), 200
 
-# this only runs if `$ python src/main.py` is exercuted
+@app.route('/contact/<contact_id>', methods=['GET', 'PUT'])
+def get_specific_contact(contact_id):
+    user1 = Contact.query.get(contact_id)
+
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+
+    body = request.get_json()
+
+    if "first_name" in body:
+        user1.first_name = body["first_name"]
+    if "last_name" in body:
+        user1.last_name = body["last_name"]
+
+
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT)
--- src/models.py
+++ src/models.py
@@ -2,16 +2,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), unique=True, nullable=False)
+    last_name = db.Column(db.String(80), unique=True, nullable=False)
+    address = db.Column(db.String(80), unique=True, nullable=False)
+    phone = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def __repr__(self):
+        return '<Contact %r>' % self.first_name
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def serialize(self):
+        return {
+            "firstName": self.first_name,
+            "email": self.email,
+            "phone": self.phone,
+            "lastName": self.last_name,
+            "address": self.address
+        }
